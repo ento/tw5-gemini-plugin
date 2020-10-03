07059020c75eff3dfe47306c293f2f45603fbbb7
ento <ento+github@i.pearlwaffles.xyz>
2020-10-03 10:43:47 -0800

    Log end of response

--- src/plugins/tw5-gemini-plugin/route-tiddler.js
+++ src/plugins/tw5-gemini-plugin/route-tiddler.js
@@ -10,6 +10,8 @@ Path: /t/TiddlerTitle
 
 const { domToGemtext, tiddlerToDom } = require('$:/plugins/ento/gemini/renderer.js');
 
+const logger = new $tw.utils.Logger('gemini-server');
+
 exports.path = /^\/t\/(.+)$/;
 
 const getTiddler = (wiki, params) => {
@@ -99,6 +101,10 @@ function renderTiddler(wiki, tiddler, response, enableTrace) {
 
   renderer(wiki, tiddler, response, enableTrace);
   response.end();
+
+  if (enableTrace) {
+    logger.log('response.end()');
+  }
 }
 exports.renderTiddler = renderTiddler;
 
